1f7e9ea8093c083bdf48a12efa736e3e4e8b0e93
Steven Lyde <lyde@cs.utah.edu>
2015-03-09 09:41:08 -0600

    Moving interpreter trait to its own file

---
+++ src/main/scala/org/ucombinator/SmallStepAbstractInterpretation.scala
@@ -0,0 +1,85 @@
+package org.ucombinator
+
+trait SmallStepAbstractInterpretation {
+  def initialState: State;
+  def next(state: State): List[State];
+
+  var printStates = false
+
+  var count = 0
+  var globalSharp: Sharp = null
+
+  def runWithGlobalSharp() {
+    // seen records the last generation store seen with this flat.
+    val seen = scala.collection.mutable.HashMap[Flat, Long]()
+    var currentGeneration: Long = 1
+
+    val init = initialState
+    var todo = List(init)
+
+    globalSharp = init.sharp
+    var timeout = -1
+
+    while (!todo.isEmpty && timeout != 0) {
+      var newState = todo.head
+      todo = todo.tail
+
+      val flat = newState.flat
+      val sharp = newState.sharp
+
+      val lastSeenGeneration: Long = seen.getOrElse(flat, 0)
+
+      if (currentGeneration <= lastSeenGeneration) {
+        // println("Current generation: " + currentGeneration) // DEBUG
+        // println("Last generation: " + lastSeenGeneration) // DEBUG
+        // println("Not a new state: " + newState) // DEBUG
+      }
+
+      if (currentGeneration > lastSeenGeneration) {
+        // globalSharp changed since we last saw this flat.
+        if (timeout > 0)
+          timeout -= 1
+
+        count += 1
+
+        if (this.printStates)
+          print("State: " + newState)
+
+        // Install the global sharp:
+        newState = (newState.sharp = globalSharp)
+
+        //println("State:\n" + newState + "\n")
+
+        // Reset the changeLog:
+        newState = (newState.sharp = newState.sharp.resetChangeLog)
+
+        // Explore successors:
+        val succs = next(newState)
+
+        // Mark this state as being seen with the current generation.
+        seen(newState.flat) = currentGeneration
+
+        // Check each successor for change:
+        for (succ <- succs) {
+          var sharp = succ.sharp
+          var delta = sharp.changeLog
+          if (!delta.isEmpty) {
+            // Something changed!
+
+            // Bump up the current sharp generation:
+            currentGeneration += 1
+
+            // Apply sharp changes to the global store:
+            globalSharp = delta(globalSharp)
+          }
+          todo = todo ++ succs
+        }
+      }
+    }
+
+    if (timeout == 0)
+      println("Timeout reached")
+
+    println("States explored: " + count)
+  }
+}
